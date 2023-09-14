d3f56187783153ddc642f2b594cd19a2962eebfa
Leejangha <jangha0423@gmail.com>
2023-09-14 17:58:59 +0900

    [unrated] Title: 카드 뭉치, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/159994. 카드 뭉치/카드 뭉치.py
+++ 프로그래머스/unrated/159994. 카드 뭉치/카드 뭉치.py
@@ -1,16 +1,10 @@
 def solution(cards1, cards2, goal):
     answer = 'Yes'
-    while goal:
-        if cards1:
-            c1 = cards1.pop(0)
-        if cards2:
-            c2 = cards2.pop(0)
-        g = goal.pop(0)
-        if g == c1:
-            cards2.insert(0, c2)
-        elif g == c2:
-            cards1.insert(0, c1)
+    for g in goal:
+        if cards1 and g == cards1[0]:
+            cards1.pop(0)
+        elif cards2 and g == cards2[0]:
+            cards2.pop(0)
         else:
             answer = 'No'
-            break
     return answer
