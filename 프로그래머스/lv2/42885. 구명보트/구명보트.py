d827cf2b3d5a3ff41bf53146c4edfff48f1d9c9f
Leejangha <jangha0423@gmail.com>
2023-10-03 12:37:14 +0900

    [level 2] Title: 구명보트, Time: 9.50 ms, Memory: 10.9 MB -BaekjoonHub

--- 프로그래머스/lv2/42885. 구명보트/구명보트.py
+++ 프로그래머스/lv2/42885. 구명보트/구명보트.py
@@ -1,10 +1,13 @@
+from collections import deque
+
 def solution(people, limit):
     answer = 0
-    people.sort()
-    MIN, MAX = 0, len(people) - 1
-    while MIN <= MAX:
-        if people[MIN] + people[MAX] <= limit:
-            MIN += 1
-        MAX -= 1
+    people = deque(sorted(people))
+    while people:
+        li = limit
+        li -= people.pop()
+        if people:
+            if li - people[0] >= 0:
+                li -= people.popleft()
         answer += 1
     return answer
