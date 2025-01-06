830151b9144e2584310c7de431d923ea536b5e62
Leejangha <jangha0423@gmail.com>
2025-01-06 14:38:51 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 0.91 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.py
+++ 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.py
@@ -1,10 +1,11 @@
 def solution(numbers):
-    # SET = set()
-    answer = []
+    # 두 수를 더해서 만들 수 있는 수는 중복이 없어야 하므로 집합 구조를 사용
+    SET = set()
     l = len(numbers)
+    # i번째 인데스 수와 i+1 부터 l까지 수들의 각각 합을 집합에 추가
     for i in range(l-1):
         for j in range(i+1,l):
-            # SET.add(numbers[i]+numbers[j])
-            answer.append(numbers[i]+numbers[j])
-    answer = sorted(set(answer))
+            SET.add(numbers[i]+numbers[j])
+    # 집합을 정렬된 리스트로 변환
+    answer = sorted(SET)
     return answer
