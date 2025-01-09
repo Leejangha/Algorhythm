bf4ccea0410d55e56d7bc297bce3051db23fdf9a
Leejangha <jangha0423@gmail.com>
2025-01-09 14:24:46 +0900

    [level 2] Title: 주식가격, Time: 27.48 ms, Memory: 19.3 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.py
+++ 프로그래머스/2/42584. 주식가격/주식가격.py
@@ -5,9 +5,13 @@ def solution(prices):
 
     # 인덱스와 함께 반복
     for i, price in enumerate(prices):
+        # 스택에 값이 있고, 현재 가격이 스택의 마지막 가격보다 떨어진 경우
         while stack and price < stack[-1][1]:
-            last = stack.pop()[0]
+            # 마지막 가격을 스택에서 빼고
+            last, _ = stack.pop()
+            # 해당 가격의 초를 현재 초에서 뺀 차이만큼 기록
             answer[last] = i - last
+        # 스택에 현재 초와 가격을 쌓음
         stack.append((i, price))
 
     # 끝까지 가격이 떨어지지 않은 기록이 stack에 남아 있으므로 전체 시간에서 뺀만큼 기록
