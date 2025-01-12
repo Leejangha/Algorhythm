4372795b938199db36f8c38190af07a400f2599f
Leejangha <jangha0423@gmail.com>
2025-01-13 00:42:39 +0900

    [level 2] Title: 기능개발, Time: 0.04 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.py
+++ 프로그래머스/2/42586. 기능개발/기능개발.py
@@ -4,29 +4,36 @@ def solution(progresses, speeds):
     answer = []
     queue = deque([])
     
+    # 작업의 진도와 속도를 함께 반복
     for p, s in zip(progresses, speeds):
-        p = 100 - p
-        if p % s == 0:
-            queue.append(p//s)
-        else:
-            queue.append(p//s + 1)
-    
-    now = queue.popleft()
-    cnt = 1
+        # 남은 작업을 속도로 나눈 몫과 나머지 계산
+        a, b = divmod(100-p, s)
+        # 나머지가 있을 경우 작업을 끝내는데 하루 더 소모됨
+        if b != 0:
+            a += 1
+        # 기능의 순서대로 큐에 작업을 끝내는데 필요한 일자를 넣음
+        queue.append(a)
+
+    # 한번의 배포되는 기능 개수 초기화
+    cnt = 0
     
     while queue:
-        if not queue:
-            answer.append(cnt)
-            now = queue.popleft()
-            cnt = 1
-        else:
-            if queue[0] <= now:
+        # 배포 완료된 첫 번째 기능
+        d = queue.popleft()
+        cnt += 1
+        
+        # 다음 기능들이 남은 경우
+        if queue:
+            # 다음 기능이 첫 번째 기능보다 먼저 완료된 경우 같이 배포
+            while queue[0] <= d:
                 queue.popleft()
                 cnt += 1
-            else:
-                answer.append(cnt)
-                now = queue.popleft()
-                cnt = 1
+                
+                if not queue:
+                    break
+        
+        # 한번에 배포 가능한 기능들의 총 개수
+        answer.append(cnt)
+        cnt = 0
 
-    answer.append(cnt)
     return answer
