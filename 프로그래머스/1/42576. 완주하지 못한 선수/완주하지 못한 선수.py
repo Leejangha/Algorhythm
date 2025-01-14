de5fa279451ea4373087912a5bd40d74597c9207
Leejangha <jangha0423@gmail.com>
2025-01-14 12:05:17 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 73.14 ms, Memory: 26.3 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,27 +1,7 @@
-# def solution(participant, completion):
-#     answer = ''
-#     par = sorted(participant)
-#     com = sorted(completion)
-#     for i in range(len(com)):
-#         if com[i] != par[i]:
-#             answer = par[i]
-#             break
-#     if not answer:
-#         answer = par[-1]
-#     return answer
 def solution(participant, completion):
-    DICT = {} 
-    for com in completion:
-        c = hash(com)
-        if c not in DICT:
-            DICT[c] = 1
-        else:
-            DICT[c] += 1
-    
-    for par in participant:
-        p = hash(par)
-        if p not in DICT or DICT[p] == 0:
-            return par
-        else:
-            DICT[p] -= 1
-    return
+    participant.sort()
+    completion.sort()
+    for i in range(len(completion)):
+        if completion[i] != participant[i]:
+            return participant[i]
+    return participant[-1]
