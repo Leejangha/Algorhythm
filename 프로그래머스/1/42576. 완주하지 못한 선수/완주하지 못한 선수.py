07ae20115b2856cddf4500851c3162d327f29346
Leejangha <jangha0423@gmail.com>
2025-01-14 14:04:47 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 41.18 ms, Memory: 37.7 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -16,16 +16,23 @@
 #             DICT[p] -= 1
 #     return
 
+# 첫번째 풀이는 value에 카운트가 들어가므로 아래 풀이가 더 해시에 적합하고 효율적인거 같음
 def solution(participant, completion):
-    DICT = {}
+    hash_table = {}
+    
+    # 해시 키 값들의 총합
     hash_sum = 0
     
+    # 참가자들을 해시 테이블에 저장
     for par in participant:
         p = hash(par)
-        DICT[p] = par
+        hash_table[p] = par
+        # 참가자들의 키 값들을 다 더함
         hash_sum += p
     
+    # 완주자들의 키 값들을 참가자들의 키 값들의 합에서 뺌
     for com in completion:
         hash_sum -= hash(com)
 
-    return DICT[hash_sum]
+    # hash_sum은 완주하지 못한 참가자의 해시 키 값이 됨
+    return hash_table[hash_sum]
