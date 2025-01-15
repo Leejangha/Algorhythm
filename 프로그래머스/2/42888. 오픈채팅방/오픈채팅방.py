737685692af7b95b8ac83cc26f8c572bd4e1b004
Leejangha <jangha0423@gmail.com>
2025-01-15 19:09:07 +0900

    [level 2] Title: 오픈채팅방, Time: 125.72 ms, Memory: 41 MB -BaekjoonHub

--- 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.py
+++ 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.py
@@ -4,30 +4,13 @@ def solution(record):
     answer = []
     nick_table = defaultdict(str)
 
-#     for r in record:
-#         if r[0] == "L":
-#             act, uid = r.split()
-#             answer.append((uid, act))
-
-#         else:
-#             act, uid, nick = r.split()
-#             if act == "Enter":
-#                 nick_table[uid] = nick
-#                 answer.append((uid, act))
-#             else:
-#                 nick_table[uid] = nick
-
-#     for i in range(len(answer)):
-#         uid, act = answer[i]
-#         if act == "Enter":
-#             answer[i] = nick_table[uid] + "님이 들어왔습니다."
-#         else:
-#             answer[i] = nick_table[uid] + "님이 나갔습니다."
+    # 방에 입장하거나 닉을 변경하는 경우 닉네임 테이블에 저장
     for r in record:
         if r[0] != "L":
             act, uid, nick = r.split()
             nick_table[uid] = nick
 
+    # 방에 입장하거나 나가는경우 닉네임 테이블에 저장된 최종 닉네임을 출력
     for r in record:
         if r[0] == "E":
             answer.append(nick_table[r.split()[1]] + "님이 들어왔습니다.")
