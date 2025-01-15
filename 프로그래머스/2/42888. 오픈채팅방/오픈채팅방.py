0b3d5b04e502c53924ce709260485aa5804cacd9
Leejangha <jangha0423@gmail.com>
2025-01-15 17:38:43 +0900

    [level 2] Title: 오픈채팅방, Time: 118.92 ms, Memory: 53.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.py
@@ -0,0 +1,27 @@
+from collections import defaultdict
+
+def solution(record):
+    answer = []
+    nick_table = defaultdict(str)
+
+    for r in record:
+        if r[0] == "L":
+            act, uid = r.split()
+            answer.append((uid, act))
+        
+        else:
+            act, uid, nick = r.split()
+            if act == "Enter":
+                nick_table[uid] = nick
+                answer.append((uid, act))
+            else:
+                nick_table[uid] = nick
+    
+    for i in range(len(answer)):
+        uid, act = answer[i]
+        if act == "Enter":
+            answer[i] = nick_table[uid] + "님이 들어왔습니다."
+        else:
+            answer[i] = nick_table[uid] + "님이 나갔습니다."
+
+    return answer
