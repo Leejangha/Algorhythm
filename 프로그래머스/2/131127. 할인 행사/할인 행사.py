beda06e104d3da1184114ee998c36e3aac5a1c00
Leejangha <jangha0423@gmail.com>
2025-01-15 17:00:13 +0900

    [level 2] Title: 할인 행사, Time: 159.02 ms, Memory: 17.2 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.py
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.py
@@ -2,8 +2,13 @@ from collections import Counter
 
 def solution(want, number, discount):
     answer = 0
+    
+    # 원하는 제품과 개수를 키 : 밸류 형식의 딕셔너리로 생성
     arr = dict(zip(want,number))
+
+    # i날짜 부터 10일 간
     for i in range(len(discount)-9):
+        # 10일 간 할인하는 제품과 개수를 카운트한 딕셔너리와 arr을 비교
         ls = Counter(discount[i:i+10])
         if arr == ls:
             answer += 1
