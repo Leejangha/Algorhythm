1382ad2eb24842acccc7c1c32056965159f75cc0
Leejangha <jangha0423@gmail.com>
2025-01-15 19:04:39 +0900

    [level 2] Title: 오픈채팅방, Time: 106.56 ms, Memory: 42.5 MB -BaekjoonHub

--- 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.py
+++ 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.py
@@ -4,24 +4,34 @@ def solution(record):
     answer = []
     nick_table = defaultdict(str)
 
+#     for r in record:
+#         if r[0] == "L":
+#             act, uid = r.split()
+#             answer.append((uid, act))
+
+#         else:
+#             act, uid, nick = r.split()
+#             if act == "Enter":
+#                 nick_table[uid] = nick
+#                 answer.append((uid, act))
+#             else:
+#                 nick_table[uid] = nick
+
+#     for i in range(len(answer)):
+#         uid, act = answer[i]
+#         if act == "Enter":
+#             answer[i] = nick_table[uid] + "님이 들어왔습니다."
+#         else:
+#             answer[i] = nick_table[uid] + "님이 나갔습니다."
     for r in record:
-        if r[0] == "L":
-            act, uid = r.split()
-            answer.append((uid, act))
-        
-        else:
+        if r[0] != "L":
             act, uid, nick = r.split()
-            if act == "Enter":
-                nick_table[uid] = nick
-                answer.append((uid, act))
-            else:
-                nick_table[uid] = nick
-    
-    for i in range(len(answer)):
-        uid, act = answer[i]
-        if act == "Enter":
-            answer[i] = nick_table[uid] + "님이 들어왔습니다."
-        else:
-            answer[i] = nick_table[uid] + "님이 나갔습니다."
+            nick_table[uid] = nick
+
+    for r in record:
+        if r[0] == "E":
+            answer.append(nick_table[r.split()[1]] + "님이 들어왔습니다.")
+        elif r[0] == "L":
+            answer.append(nick_table[r.split()[1]] + "님이 나갔습니다.")
 
     return answer
