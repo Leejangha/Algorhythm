035a18188d4fcf32c40ad88a79f2c19fe2d9bf67
Leejangha <jangha0423@gmail.com>
2025-01-21 18:12:53 +0900

    [level 3] Title: 베스트앨범, Time: 0.07 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
@@ -1,8 +1,12 @@
 def solution(genres, plays):
     answer = []
+    
+    # 장르별로 노래를 담아 재생수로 정렬할 딕셔너리
     DICT = {}
+    # 장르별 재생 횟수를 담을 딕셔너리
     sum_plays = {}
     
+    # 기준 3번에 고유번호를 봐야하므로 인덱스도 같이 반복
     for i, (g, p) in enumerate(zip(genres, plays)):
         if g not in DICT:
             DICT[g] = [(i,p)]
@@ -11,15 +15,15 @@ def solution(genres, plays):
             sum_plays[g] += p
             DICT[g].append((i,p))
 
+    # 재생된 횟수를 기준으로 내림차순 정렬
     sum_plays = sorted(sum_plays.items(), key=lambda x : x[1], reverse=True)
     
+    # 많이 재생된 장르의 노래부터 탐색
     for genre, _ in sum_plays:
-        songs = sorted(DICT[genre], key=lambda x : x[1], reverse=True)
+        # 같은 장르 내에서 재생된 횟수를 기준으로 내림차순 정렬(처음 리스트에 넣을때 인덱스 순으로 넣었으므로 횟수가 같을 경우 낮은 인덱스가 먼저임)
+        songs = sorted(DICT[genre], key=lambda x : x[1], reverse=True)[:2]
         
-        if len(songs) == 1:
-            answer.append(songs[0][0])
-        else:
-            for i in range(2):
-                answer.append(songs[i][0])
+        for song, _ in songs:
+            answer.append(song)
     
     return answer
