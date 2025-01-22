5654439b4dde273c1376453e968b6d7c4664bdd7
Leejangha <jangha0423@gmail.com>
2025-01-22 11:20:14 +0900

    [level 1] Title: 신고 결과 받기, Time: 164.15 ms, Memory: 52.2 MB -BaekjoonHub

--- 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.py
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.py
@@ -1,17 +1,25 @@
 def solution(id_list, report, k):
-    answer = [0] * len(id_list)
-    id_dict = {}
-    for id in id_list:
-        id_dict[id] = 0
-    reports = set(report)
-    mail = []
-    for r in reports:
-        user, decl = r.split()
-        mail.append((user,decl))
-        id_dict[decl] += 1
-    for decl, cnt in id_dict.items():
-        if cnt >= k:
-            for m in mail:
-                if m[1] == decl:
-                    answer[id_list.index(m[0])] += 1
+    # 정지시킨 유저수를 계산할 딕셔너리
+    answer = {id : 0 for id in id_list}
+    
+    # 신고당한 유저별로 신고한 유저를 담을 딕셔너리
+    DICT = {id : set() for id in id_list}
+    
+    # 동일한 유저에 대한 신고는 1회로 처리하므로 중복제거
+    report = set(report)
+    
+    for rep in report:
+        a, b = rep.split()
+        # 나를 신고한 유저를 담음
+        DICT[b].add(a)
+    
+    for key, value in DICT.items():
+        # 신고를 k번 이상 당해 정지된 유저
+        if len(value) >= k:
+            # 정지된 유저를 신고한 유저의 결과 +1
+            for val in value:
+                answer[val] += 1
+    
+    # 정지시킨 결과 수를 리스트로 변환
+    answer = list(answer.values())
     return answer
