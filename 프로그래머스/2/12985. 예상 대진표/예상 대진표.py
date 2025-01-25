8ea6f98b677a72cad78fcc747493a944228a6d05
Leejangha <jangha0423@gmail.com>
2025-01-26 01:57:09 +0900

    [level 2] Title: 예상 대진표, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/12985. 예상 대진표/예상 대진표.py
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.py
@@ -1,10 +1,9 @@
 def solution(n,a,b):
-    answer = 0
-    for i in range(n):
-        a = (a + 1)//2
-        b = (b + 1)//2
-        answer += 1
-        if abs(a - b) == 0:
-            break
-               
-    return answer
+    for i in range(1, n//2 + 1):
+        a = (a+1)//2
+        b = (b+1)//2
+        
+        if a == b:
+            return i
+
+    return
