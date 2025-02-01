96cb271d2b95aa376561356d4aaad871e1e633f6
Leejangha <jangha0423@gmail.com>
2025-02-01 14:10:27 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.03 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.py
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.py
@@ -1,13 +1,8 @@
 def solution(n, words):
-    SET = {words[0]}
-    turn = 1
-    tail = words[0][-1]
-    for word in words[1:]:
-        turn += 1
-        head = word[0]
-        if word in SET or head != tail:
-            return [turn % n if turn % n != 0 else n, (turn-1) // n + 1]
-        else:
-            SET.add(word)
-        tail = word[-1]
-    return [0, 0]
+    SET = set()
+    SET.add(words[0])
+    for i in range(1, len(words)):
+        if words[i-1][-1] != words[i][0] or words[i] in SET:
+            return [i%n+1, i//n +1]
+        SET.add(words[i])
+    return [0,0]
