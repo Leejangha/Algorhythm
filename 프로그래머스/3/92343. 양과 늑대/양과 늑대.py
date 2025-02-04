b4ec43c53024728c12bdb4208b3fc9bfee8a55eb
Leejangha <jangha0423@gmail.com>
2025-02-04 20:36:19 +0900

    [level 3] Title: 양과 늑대, Time: 11.54 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/3/92343. 양과 늑대/양과 늑대.py
+++ 프로그래머스/3/92343. 양과 늑대/양과 늑대.py
@@ -1,28 +1,29 @@
 def solution(info, edges):
-    global answer
-    answer = 0
-    graph =  [[] for _ in range(len(info))]
-    for a, b in edges:
-        graph[a].append(b)
-        
-    def dfs(now, nxt, sheep, wolf):
-        global answer
-        
-        if info[now] == 0:
-            sheep += 1
-            answer = max(answer, sheep)
-        else:
-            wolf += 1
-        
-        if sheep <= wolf:
-            return 0
-        
-        for n in nxt:
-            for node in graph[n]:
-                if node not in nxt:
-                    nxt.append(node)
-                    dfs(node, nxt, sheep, wolf)
-                    nxt.pop()
+    answer = []
+    # 방문한 노드를 체크할 리스트
+    visited = [False] * len(info)
     
-    dfs(0, [0], 0, 0)
-    return answer
+    # 깊이 우선 탐색 방법으로 이동할 수 있는 노드를 탐색
+    def dfs(sheeps, wolves):
+        # 양이 늑대보다 많은 경우 현재 양의 개수를 리스트에 저장
+        if sheeps > wolves:
+            answer.append(sheeps)
+        # 늑대가 양보다 같거나 많을 경우 종료
+        else:
+            return
+        
+        for p, c in edges:
+            # 부모는 방문하였지만 자식은 방문 안한 경우만 진행
+            if visited[p] and not visited[c]:
+                visited[c] = True
+                if info[c] == 0:
+                    dfs(sheeps + 1, wolves)
+                else:
+                    dfs(sheeps, wolves + 1)
+                # 만약 다른 곳을 들렸다 방문 시 조건에 충족할 수 있어 false 처리
+                visited[c] = False
+                
+    visited[0] = True
+    dfs(1, 0)
+    
+    return max(answer)
