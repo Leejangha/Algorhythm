e2b1f3ef289eb58ad24d924eda4dc57b566f44c4
Leejangha <jangha0423@gmail.com>
2025-02-04 20:01:24 +0900

    [level 2] Title: 예상 대진표, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/12985. 예상 대진표/예상 대진표.py
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.py
@@ -1,8 +1,10 @@
 def solution(n,a,b):
     for i in range(1, n//2 + 1):
+        # 해당 라운드에서 몇번째 게임을 하는지 계산
         a = (a+1)//2
         b = (b+1)//2
         
+        # 같은 게임을 하는경우 서로가 맞붙음
         if a == b:
             return i
 
