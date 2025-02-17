44396129dad6b7c5bc8f5dcfaf1a87c608779cb5
Leejangha <jangha0423@gmail.com>
2025-02-17 19:26:24 +0900

    [level 2] Title: 배달, Time: 39.49 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/2/12978. 배달/배달.py
+++ 프로그래머스/2/12978. 배달/배달.py
@@ -2,21 +2,26 @@ from collections import defaultdict
 
 
 def solution(N, road, K):
+    # 인접 리스트 만들기
     graph = defaultdict(list)
     
+    # 무방향 그래프
     for a, b, c in road:
         graph[a].append((b,c))
         graph[b].append((a,c))
 
+    # 거리 배열 초기화
     distance = [float("inf")] * (N+1)
     distance[1] = 0
     
+    # 노드 수 만큼 반복
     for _ in range(N):
         for a in range(1, N+1):
             for b, c in graph[a]:
+                 # 현재 노드 a를 거쳐 b로 가는 경로의 거리가 기존의 b까지의 거리보다 짧은 경우
                 if distance[a] + c < distance[b]:
+                    # 최단거리 갱신
                     distance[b] = distance[a] + c
-                    
-    answer = len(list(x for x in distance if x <= K))
-
-    return answer
+    
+    # K 시간 이하로 배달이 가능한 마을의 수
+    return len(list(x for x in distance if x <= K))
