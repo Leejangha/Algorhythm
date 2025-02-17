488037c5149b41c08ca8d9d363aaf74e823327b4
Leejangha <jangha0423@gmail.com>
2025-02-17 19:57:46 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 5.18 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.py
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.py
@@ -1,27 +1,35 @@
-from collections import deque
+from collections import defaultdict, deque
 
 
 def solution(n, wires):
-    answer = float("inf")
-    trees = [[] for _ in range(n+1)]
-    for wire in wires:
-        trees[wire[0]].append(wire[1])
-        trees[wire[1]].append(wire[0])
-
-    def bfs(start, no, n):
+    # 트리 생성
+    tree = defaultdict(list)
+    for a, b in wires:
+        tree[a].append(b)
+        tree[b].append(a)
+    
+    # 너비 우선 탐색으로 연결을 끊은 전력망에서 start 송전탑이 포함된 전력망 내의 송전탑 개수를 셈
+    def bfs(start, ban, n):
         q = deque()
         q.append(start)
+        # 방문 여부
         visited = [False] * (n+1)
         while q:
             now = q.popleft()
             visited[now] = True
-            for next in trees[now]:
-                if next != no and not visited[next]:
-                    visited[next] = True
-                    q.append(next)
+            for nxt in tree[now]:
+                # 다음 노드가 전선을 끊을 송전탑이 아니고 방문한 적이 없을 때
+                if nxt != ban and not visited[nxt]:
+                    visited[nxt] = True
+                    q.append(nxt)
+        # 방문을 한 노드 == 연결이 되어있는 송전탑 개수
         return visited.count(True)
 
-    for wire in wires:
-        answer = min(answer, abs(bfs(wire[0], wire[1], n) - bfs(wire[1], wire[0], n)))
+    # 모든 송전탑이 연결된 상태로 초기화
+    answer = n
+    
+    # 연결된 전선을 끊었을 때 두 전력망의 송전탑 개수 차가 최소가 되는 정답 찾기
+    for a, b in wires:
+        answer = min(answer, abs(bfs(a, b, n) - bfs(b, a, n)))
+    
     return answer
-
