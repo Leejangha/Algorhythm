da1a17095464fe55c86500eaaafc2758a89d814b
Leejangha <jangha0423@gmail.com>
2025-02-21 18:04:40 +0900

    [level 2] Title: 피로도, Time: 25.42 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.py
+++ 프로그래머스/2/87946. 피로도/피로도.py
@@ -21,3 +21,30 @@ def solution(k, dungeons):
             answer = cnt
     
     return answer
+
+
+# # 2. 백트래킹
+# def solution(k, dungeons):
+#     answer = -1
+#     N = len(dungeons)
+#     visited = [False] * N
+    
+#     def back(k, cnt, dungeons):
+#         nonlocal answer
+#         # 탐험한 최대 던전 수 업데이트
+#         if cnt > answer:
+#             answer = cnt
+        
+#         # 던전의 방문여부를 기록해야 하므로 인덱스를 같이
+#         for i, (need, spend) in enumerate(dungeons):
+#             # 던전의 필요 피로도보다 많이 남아있고 아직 방문하지 않은 경우
+#             if k >= need and not visited[i]:
+#                 # 방문처리
+#                 visited[i] = True
+#                 # 피로도를 깎고 탐험한 던전수를 늘려서 아직 탐험하지 않은 다음 던전들을 탐험
+#                 back(k - spend, cnt + 1, dungeons)
+#                 # 뒤에 던전을 먼저 탐험한 뒤에 앞에 던전을 탐험할 수 있으니 재방문처리
+#                 visited[i] = False
+
+#     back(k, 0, dungeons)
+#     return answer
