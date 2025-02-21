f7095a36689f2ad1dd4ad98afb37cdc8d4b46ae5
Leejangha <jangha0423@gmail.com>
2025-02-21 16:12:25 +0900

    [level 2] Title: 피로도, Time: 24.90 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.py
+++ 프로그래머스/2/87946. 피로도/피로도.py
@@ -1,9 +1,11 @@
+# 1. 완전탐색
 from itertools import permutations
 
 
 def solution(k, dungeons):
     answer = -1
     
+    # 던전들의 모든 순열을 생성후 완전탐색
     for perm in permutations(dungeons, len(dungeons)):
         hp = k
         cnt = 0
@@ -12,6 +14,8 @@ def solution(k, dungeons):
             if hp >= need:
                 hp -= spend
                 cnt += 1
+            else:
+                break
         
         if cnt > answer:
             answer = cnt
