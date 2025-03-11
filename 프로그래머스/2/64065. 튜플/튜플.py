3c586a96550a96c7bb6db8ae3d3159a296c83749
Leejangha <jangha0423@gmail.com>
2025-03-11 21:57:24 +0900

    [level 2] Title: 튜플, Time: 329.69 ms, Memory: 12 MB -BaekjoonHub

---
+++ 프로그래머스/2/64065. 튜플/튜플.py
@@ -0,0 +1,12 @@
+def solution(s):
+    answer = []
+    tuples = sorted(list(s[2:-2].split("},{")), key = lambda x : len(x))
+    for idx, tup in enumerate(tuples):
+        tup = list(tup.split(","))
+        i = 0
+        while i <= idx:
+            if int(tup[i]) not in answer:
+                answer.append(int(tup[i]))
+            i += 1
+                
+    return answer
