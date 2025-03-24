2d96003874a4980a920b99fb7185a305ff56d2ac
Leejangha <jangha0423@gmail.com>
2025-03-24 14:36:27 +0900

    [level 2] Title: 카펫, Time: 0.20 ms, Memory: 9.23 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.py
+++ 프로그래머스/2/42842. 카펫/카펫.py
@@ -1,7 +1,10 @@
 def solution(brown, yellow):
+    # xy = yellow, x >= y --> yellow >= y^2
     for y in range(1, int(yellow ** 0.5) + 1):
+        # 길이는 정수
         if yellow % y == 0:
             x = yellow // y
+            # 갈색은 노란색 가로 세로 합의 두배에 모서리 4를 더한 값
             if 2 * (x + y) == brown - 4:
                 return [x + 2, y + 2]
     return
