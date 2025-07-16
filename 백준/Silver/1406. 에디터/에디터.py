ab692b3d2eff41907349b79848012b41bb490584
Leejangha <jangha0423@gmail.com>
2025-07-16 15:21:02 +0900

    [Silver II] Title: 에디터, Time: 416 ms, Memory: 95760 KB -BaekjoonHub

--- 백준/Silver/1406. 에디터/에디터.py
+++ 백준/Silver/1406. 에디터/에디터.py
@@ -1,17 +1,19 @@
-front = list(input())
-M = int(input())
+import sys
+
+front = list(sys.stdin.readline())[:-1]
+M = int(sys.stdin.readline())
+operators = [sys.stdin.readline().split() for _ in range(M)]
 
 back = []
 
-for _ in range(M):
-    op = input()
-    if op == "L" and front:
+for op in operators:
+    if op[0] == "L" and front:
         w = front.pop()
         back.append(w)
-    elif op == "D" and back:
+    elif op[0] == "D" and back:
         w = back.pop()
         front.append(w)
-    elif op == "B" and front:
+    elif op[0] == "B" and front:
         front.pop()
     elif op[0] == "P":
         w = op[-1]
