35fc44d1a696fb58ca0852b1bbddcc2b46893f6b
Leejangha <jangha0423@gmail.com>
2025-09-08 16:08:03 +0900

    [Bronze II] Title: 분산처리, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/1009. 분산처리/분산처리.py
+++ 백준/Bronze/1009. 분산처리/분산처리.py
@@ -1,22 +1,14 @@
 import sys
 
-N = int(input())
-
-DICT = {"1" : [1],
-        "2" : [2,4,8,6],
-        "3" : [3,9,7,1],
-        "4" : [4,6],
-        "5" : [5],
-        "6" : [6],
-        "7" : [7,9,3,1],
-        "8" : [8,4,2,6],
-        "9" : [9,1],
-}
+N = int(sys.stdin.readline())
 
 for _ in range(N):
-    a, b = map(int, input().split())
-    a = str(a)[-1]
-    if a == "0":
+    a, b = map(int, sys.stdin.readline().split())
+    b = b%4
+    if b == 0:
+        b=4
+    comp = str(a**b)[-1]
+    if comp == "0":
         print(10)
     else:
-        print(DICT[a][b%len(DICT[a])-1])
+        print(comp)
